c1a2ac963e0d313c7b3d9f316e0125bca0c6bedd
nickfogle <nickfogle@gmail.com>
2014-12-02 21:17:48 -0800

    modifying terminal logic

--- js/dterm.js
+++ js/dterm.js
@@ -1,6 +1,6 @@
 /*!
  * Example plugin using JQuery Terminal Emulator
- * Copyright (C) 2010 Jakub Jankiewicz <http://jcubic.pl> 
+ * Copyright (C) 2010 Jakub Jankiewicz <http://jcubic.pl>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -11,53 +11,86 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 (function($) {
-    $.extend_if_has = function(desc, source, array) {
-        for (var i=array.length;i--;) {
-            if (typeof source[array[i]] != 'undefined') {
-                desc[array[i]] = source[array[i]];
-            }
-        }
-        return desc;
-    };
-    $.fn.dterm = function(eval, options) {
-        var op = $.extend_if_has({}, options, 
-                                   ['greetings', 'prompt', 'onInit',
-                                    'onExit', 'clear',
-                                    'login', 'name', 'exit']);
-        op.enabled = false;
-        var terminal = this.terminal(eval, op).css('overflow', 'hidden');
-        if (!options.title) {
-            options.title = 'JQuery Terminal Emulator';
-        }
-        if (options.logoutOnClose) {
-            options.close = function(e, ui) {
-                terminal.logout();
-                terminal.clear();
-            };
-        } else {
-            options.close = function(e, ui) {
-                terminal.disable();
-            };
-        }
-        var self = this;
-        this.dialog($.extend(options, {
-            resizeStop: function(e, ui) {
-                var content = self.find('.ui-dialog-content');
-                terminal.resize(content.width(), content.height());
-            },
-            open: function(e, ui) {
-                terminal.focus();
-                terminal.resize();
-            },
-            show: 'fade',
-            closeOnEscape: false
-        }));
-        self.terminal = terminal;
-        return self;
-    };
+   $.extend_if_has = function(desc, source, array) {
+      for (var i = array.length; i--;) {
+         if (typeof source[array[i]] != 'undefined') {
+            desc[array[i]] = source[array[i]];
+         }
+      }
+      return desc;
+   };
+
+   var controller = {};
+
+   controller.global = {
+
+      konami: function() {
+         action.konami(this)
+      },
+
+      version: function() {
+         this.echo("\nOMC v2.0 - 2013-03-09\n\n")
+      },
+      copyright: function() {
+         var e = (new Date).getFullYear() + 1;
+         this.echo(t)
+      },
+      man: function(e) {
+         switch (e) {
+            case undefined:
+               this.error("man command except at least one argument.");
+               break;
+               break;
+            default:
+               this.error("man command not implemented yet. Sorry about that !");
+               action.cry(this)
+         }
+      },
+      "..": function() {
+         this.exec("exit", true)
+      }
+   };
+
+
+   $.fn.dterm = function(eval, options) {
+      var op = $.extend_if_has({}, options, ['greetings', 'prompt', 'onInit',
+         'onExit', 'clear',
+         'login', 'name', 'exit'
+      ]);
+      op.enabled = false;
+      var terminal = this.terminal(eval, op).css('overflow', 'hidden');
+      if (!options.title) {
+         options.title = 'JQuery Terminal Emulator';
+      }
+      if (options.logoutOnClose) {
+         options.close = function(e, ui) {
+            terminal.logout();
+            terminal.clear();
+         };
+      } else {
+         options.close = function(e, ui) {
+            terminal.disable();
+         };
+      }
+      var self = this;
+      this.dialog($.extend(options, {
+         resizeStop: function(e, ui) {
+            var content = self.find('.ui-dialog-content');
+            terminal.resize(content.width(), content.height());
+         },
+         open: function(e, ui) {
+            terminal.focus();
+            terminal.resize();
+         },
+         show: 'fade',
+         closeOnEscape: false
+      }));
+      self.terminal = terminal;
+      return self;
+   };
 })(jQuery);
